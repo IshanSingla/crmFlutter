4f6f4e143d8f8f571d13df44b2cdb0fa8c3b35bb
VanshArora08 <kumar.vansharora@gmail.com>
2023-01-20 18:58:21 +0530

    login page edited

--- lib/Screens/login/login2.dart
+++ lib/Screens/login/login2.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class MyColors{
+class MyColors {
   static Color blue = Color.fromARGB(255, 0, 117, 176);
   static Color cream = Color.fromARGB(255, 249, 242, 237);
   static Color pink = Color.fromARGB(255, 248, 116, 116);
-  static Color mustard = Color.fromARGB(255, 181, 98, 1);
+  static Color mustard = Color.fromARGB(255, 255, 181, 98);
 }
 
 class loginString2 extends StatefulWidget {
@@ -44,7 +44,7 @@ class _loginString2State extends State<loginString2> {
                 color: Colors.white),
             child: Padding(
               padding: EdgeInsets.symmetric(
-                  horizontal: 5 * deviceWidthr, vertical: 10 * deviceHeightr),
+                  horizontal: 5 * deviceWidthr, vertical: 8 * deviceHeightr),
               child: Form(
                   key: _formKey,
                   child: Column(children: [
@@ -77,7 +77,10 @@ class _loginString2State extends State<loginString2> {
                     SizedBox(height: 5 * deviceHeightr),
                     ElevatedButton(
                       onPressed: () => moveToHome(context),
-                      child: Text("Sign in"),
+                      child: Text(
+                        "Sign in",
+                        style: TextStyle(color: Colors.black),
+                      ),
                       style: ElevatedButton.styleFrom(
                           backgroundColor: MyColors.mustard),
                     ),
